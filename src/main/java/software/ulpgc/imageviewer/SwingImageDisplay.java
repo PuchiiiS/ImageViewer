bd36d34ab460947291edf1a1200fa8d1455be391
Estados <hsuliman@gmail.com>
2025-01-03 15:53:45 +0000

    Class Resizer Created and updated SwingImageDisplay

--- src/main/java/software/ulpgc/imageviewer/SwingImageDisplay.java
+++ src/main/java/software/ulpgc/imageviewer/SwingImageDisplay.java
@@ -3,9 +3,15 @@ package software.ulpgc.imageviewer;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
+
+import static java.lang.Math.min;
+import static java.lang.Math.round;
 
 public class SwingImageDisplay extends JPanel implements ImageDisplay {
     private Image image;
@@ -25,25 +31,35 @@ public class SwingImageDisplay extends JPanel implements ImageDisplay {
 
     @Override
     public void paint(Graphics g) {
-        g.setColor(Color.BLACK);
+        g.setColor(Color.DARK_GRAY);
+        Resizer resizer = new Resizer(bitmap);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
-        Resizer resizer = new Resizer(new Dimension(this.getWidth(), this.getHeight()));
-        Dimension resized = resizer.resize(new Dimension(bitmap.getWidth(), bitmap.getHeight()));
-        int x = (this.getWidth() - bitmap.getWidth()) / 2;
-        int y = (bitmap.getHeight() - bitmap.getHeight()) / 2;
-        g.drawImage(bitmap, x, y, null);
+        BufferedImage resized = resizer.resize(scaleFactor());
+        int x = (this.getWidth() - resized.getWidth()) / 2;
+        int y = (this.getHeight() - resized.getHeight()) / 2;
+        g.drawImage(resized, x, y, null);
+        File file = new File(this.image.name());
+        g.drawString(printString(file.getName()), 0, this.getHeight() - 6);
+        double scaleFactor = scaleFactor();
+        String percentage = "100%";
+        if (scaleFactor < 1) {
+            percentage = round(scaleFactor*100) + "%";
+        }
+        g.drawString(printString(percentage), this.getWidth() - 50, this.getHeight() - 6);
     }
 
-    public static class Resizer {
-        private final Dimension dimension;
+    private AttributedCharacterIterator printString(String string) {
+        AttributedString name = new AttributedString(string);
+        name.addAttribute(TextAttribute.SIZE, 18);
+        name.addAttribute(TextAttribute.FONT, Font.SANS_SERIF);
+        name.addAttribute(TextAttribute.FOREGROUND, Color.WHITE);
+        return name.getIterator();
+    }
 
-        public Resizer(Dimension dimension) {
-            this.dimension = dimension;
-        }
-
-        public Dimension resize(Dimension dimension) {
-            return null;
-        }
+    private double scaleFactor() {
+        double widthFactor = (double) this.getWidth() / bitmap.getWidth();
+        double heightFactor = (double) (this.getHeight() - 50) / bitmap.getHeight();
+        return min(widthFactor, heightFactor);
     }
 
     private BufferedImage load(String name) {
